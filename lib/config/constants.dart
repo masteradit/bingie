754cf8129769b00130d800052206c30906ba54a2
Master Adit <aditluhadia@gmail.com>
2020-07-04 18:26:36 +0530

    Set up theme and added colors

---
+++ lib/config/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const Color PrimaryColorD =
+      Color.fromRGBO(1, 111, 185, 1); // Spanish Blue
+  static const Color AccentColorD =
+      Color.fromRGBO(35, 174, 209, 1); // Pacific Blue
+  static const Color BackgroundColorD =
+      Color.fromRGBO(24, 40, 37, 1); // Dark Jungle Green
+  static const Color PrimaryTextColorD =
+      Color.fromRGBO(248, 244, 227, 1); // Eggshell
+  static const Color AccentTextColorD =
+      Color.fromRGBO(255, 255, 255, 1); // White
+}
